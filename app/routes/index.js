af0916082d3598d1bcd68239f9054cc2e4f5ae5a
Bruno Justino <bjpraciano@gmail.com>
2018-02-02 07:33:43 -0200

    Config view and controllers

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,5 @@
 module.exports = function(application){
-    application.post('/chat',function(req,res){
-        res.render("chat");
-    });
-
     application.get('/',function(req,res){
-        res.render("index");
-    });
-
-    application.get('/chat',function(req,res){
-        res.render("chat");
+        application.app.controllers.index.home(application,req,res);
     });
 }
