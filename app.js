c59b43dfe5d7d432567d6f42e24d5d54616c7117
Bruno Justino <bjpraciano@gmail.com>
2018-02-02 08:57:35 -0200

    Including web sockets

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var app = require('./config/server');
 
-app.listen(3000, function(){
+var server = app.listen(3000, function(){
     console.log('servidor online');
 });
 
+var io = require('socket.io').listen(server);
+
+io.on('connection',function(socket){
+    console.log('oi');
+    socket.on('disconnect',function(){
+        console.log('Usuário desconectou');
+    });
+});
